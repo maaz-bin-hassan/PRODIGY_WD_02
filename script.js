cb843805584e34571c0f4e5ccf55f00594c461a3
maaz bin Hassan <mleghari64@gmail.com>
2024-01-17 13:20:06 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+  
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+  
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+  
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+  
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+  
+    return `${formattedMM}:${formattedSS}:${formattedMS}`;
+  }
+  
+  
+  
+  let startTime;
+  let elapsedTime = 0;
+  let timerInterval;
+  
+ 
+  
+  function print(txt) {
+    document.getElementById("display").innerHTML = txt;
+  }
+  
+  
+  
+  function start() {
+    startTime = Date.now() - elapsedTime;
+    timerInterval = setInterval(function printTime() {
+      elapsedTime = Date.now() - startTime;
+      print(timeToString(elapsedTime));
+    }, 10);
+    showButton("PAUSE");
+  }
+  
+  function pause() {
+    clearInterval(timerInterval);
+    showButton("PLAY");
+    addLapTime(elapsedTime);
+  }
+  
+  
+  function reset() {
+    clearInterval(timerInterval);
+    print("00:00:00");
+    elapsedTime = 0;
+    lapCount = 1;
+    showButton("PLAY");
+    lapList.innerHTML = ''; 
+  }
+  
+  
+
+  
+  function showButton(buttonKey) {
+    const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+    const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+    buttonToShow.style.display = "block";
+    buttonToHide.style.display = "none";
+  }
+  // Create event listeners
+  
+  let playButton = document.getElementById("playButton");
+  let pauseButton = document.getElementById("pauseButton");
+  let resetButton = document.getElementById("resetButton");
+  
+  playButton.addEventListener("click", start);
+  pauseButton.addEventListener("click", pause);
+  resetButton.addEventListener("click", reset);
+
+  let lapCount = 1;
+let lapList = document.getElementById('lapList');
+
+function addLapTime(elapsedTime) {
+  const lapTime = timeToString(elapsedTime);
+  const lapItem = document.createElement('li');
+  lapItem.textContent = `Lap ${lapCount}: ${lapTime}`;
+  lapList.appendChild(lapItem);
+  lapCount++;
+}
